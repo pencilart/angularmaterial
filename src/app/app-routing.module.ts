5eb9f13f90ca93905807bb4ede4f8e8524213a7a
Vijay <pencilartcanvas@gmail.com>
2022-02-10 23:56:37 +0530

    material project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard' , component: DashboardComponent},
+  { path: 'home' , component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
